bcdee1144dcd6fcb6ca9c50c563179d03537bf1d
jonnyACCI <jshan9078@gmail.com>
2023-09-03 15:21:50 -0400

    testing 2

--- backend/index.js
+++ backend/index.js
@@ -19,30 +19,30 @@ mongoose.connect(process.env.MONGO_URI, {})
 
 app.use(function(req, res, next) {
     var oneof = false;
-    console.log(req.headers);
-    if(req.headers.origin) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-method']) {
-        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-headers']) {
-        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-        oneof = true;
-    }
-    if(oneof) {
-        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-    }
+    console.log(req);
+    // if(req.headers.origin) {
+    //     res.header('Access-Control-Allow-Origin', req.headers.origin);
+    //     oneof = true;
+    // }
+    // if(req.headers['access-control-request-method']) {
+    //     res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+    //     oneof = true;
+    // }
+    // if(req.headers['access-control-request-headers']) {
+    //     res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+    //     oneof = true;
+    // }
+    // if(oneof) {
+    //     res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    // }
 
-    // intercept OPTIONS method
-    if (oneof && req.method == 'OPTIONS') {
-        res.send(200);
-    }
-    else {
-        next();
-    }
+    // // intercept OPTIONS method
+    // if (oneof && req.method == 'OPTIONS') {
+    //     res.send(200);
+    // }
+    // else {
+    //     next();
+    // }
 });
 
 
