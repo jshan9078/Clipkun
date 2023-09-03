a265b9557950fd1f8ac733a2b463f1df3eb57bb1
jonnyACCI <jshan9078@gmail.com>
2023-09-03 03:31:47 -0400

    updated origin

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,20 @@ mongoose.connect(process.env.MONGO_URI, {})
     console.log(err);
 });
 
-app.use(cors(
-    {
-        origin: [""],
+var allowedOrigins = ['https://clipkun.vercel.app'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
     }
-));
+    return callback(null, true);
+  }
+}));
 
 //middleware
 app.use(express.json());
