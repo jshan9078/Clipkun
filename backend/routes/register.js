cbd8cb27304af339a1927f07e7b60cdfa0ed3f8d
jonnyACCI <jshan9078@gmail.com>
2023-09-03 16:14:11 -0400

    testing 16

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -6,9 +6,12 @@ const cors=require('cors');
 
 
 router.post('/',async (req, res,next) => {
-    res.append("Access-Control-Allow-Origin", ["*"]);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     next();
     let {name, password} = req.body;
     const arr = await User.find({});
