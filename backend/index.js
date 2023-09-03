6e9d2796be0c80cd3c9d6edfbd2cd235baca6453
jonnyACCI <jshan9078@gmail.com>
2023-09-03 15:47:00 -0400

    testing 7

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {})
 //middleware
 
 AudioParamMap.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://clipkun.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://clipkun.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
