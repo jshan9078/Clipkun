fd342af4e56f4232df79bd7c9bc81f77bd796203
jonnyACCI <jshan9078@gmail.com>
2023-09-03 15:53:43 -0400

    testing 10

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -7,7 +7,7 @@ const cors=require('cors');
 router.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'https://clipkun.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Origin');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
