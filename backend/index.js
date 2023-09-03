d5708769de43ac85d61088b114d5a1bb6de8f899
jonnyACCI <jshan9078@gmail.com>
2023-09-03 14:11:27 -0400

    added cors

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,9 @@ mongoose.connect(process.env.MONGO_URI, {})
 //middleware
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 
-  
+
 app.use(express.json());
 
 
