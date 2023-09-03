6bc79ca8bd148ca9a799c2476fc506834d6d871f
jonnyACCI <jshan9078@gmail.com>
2023-09-03 03:23:44 -0400

    changed origin

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI, {})
 
 app.use(cors(
     {
-        origin: ["https://clipkun-server.vercel.app"],
+        origin: [""],
         credentials: true
     }
 ));
