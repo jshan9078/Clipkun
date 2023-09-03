66d42fcfd075cb8dfd9db035aae5d258ba0f52e2
jonnyACCI <jshan9078@gmail.com>
2023-09-03 15:27:22 -0400

    testing 3

--- backend/index.js
+++ backend/index.js
@@ -17,34 +17,13 @@ mongoose.connect(process.env.MONGO_URI, {})
 
 //middleware
 
-app.use(function(req, res, next) {
-    var oneof = false;
-    console.log(req);
-    // if(req.headers.origin) {
-    //     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    //     oneof = true;
-    // }
-    // if(req.headers['access-control-request-method']) {
-    //     res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-    //     oneof = true;
-    // }
-    // if(req.headers['access-control-request-headers']) {
-    //     res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-    //     oneof = true;
-    // }
-    // if(oneof) {
-    //     res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-    // }
-
-    // // intercept OPTIONS method
-    // if (oneof && req.method == 'OPTIONS') {
-    //     res.send(200);
-    // }
-    // else {
-    //     next();
-    // }
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
 });
-
 
 
 
